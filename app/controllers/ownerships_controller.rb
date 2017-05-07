8c0ba6d47cb66a0a6a4db78aafe81e705df5cc07
jon tee <purai3564ef@xvb.biglobe.ne.jp>
2017-05-07 13:38:49 +0000

    heroku56

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -23,12 +23,12 @@ class OwnershipsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:item_id])
-
+    
     if params[:type] == 'Want'
-      current_user.unwant(@item) 
+      current_user.unwant(@item)
       flash[:success] = '商品の Want を解除しました。'
     elsif params[:type] == 'Have'
-      current_user.unhave(@item) 
+      current_user.unhave(@item)
       flash[:success] = '商品の Have を解除しました。'
     end
     redirect_back(fallback_location: root_path)  
